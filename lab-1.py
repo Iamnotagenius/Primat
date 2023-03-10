c294242448749bfccbe16c6ad47cbb2a49cea55b
Iamnotagenius <kazakk9@gmail.com>
2023-03-10 16:15:28 +0300

    use docstrings

--- lab-1.py
+++ lab-1.py
@@ -1,23 +1,26 @@
 # -------------------------------------------
 # Oбязательные входные данные
 
-# (левая граница, правая граница, шаг)
 a = -1
-b = 1
-h = 0.01
+"""Левая граница"""
 
-# Сама функция
+b = 1
+"""Правая граница"""
+
+h = 0.01
+"""Шаг"""
 
 
 def func(x: float):
+    """Функция для оптимизации"""
     return x ** 2
 
 # --------------------------------------------
 # Далее все инструменты для вычисления
 
 
-# Шаг сетки
 n = round((a - b) / h)
+"""Шаг сетки"""
 
 # Значения x от i
 
@@ -29,73 +32,64 @@ def getXOnIteration(i: int):
 
 # --Производные--
 
-# Нахождение правой разностной производной в точке
-
 
 def rightDifferenceDerivative(x):
+    """Нахождение правой разностной производной в точке"""
     return (func(x + h) - func(x)) / h
-
-# Находение левой разностной производной в точке
 
 
 def leftDifferenceDerivative(x):
+    """Находение левой разностной производной в точке"""
     return (func(x) - func(x - h)) / h
-
-# Более точная производная в точке при задействии трёх узлов
 
 
 def derivativeWithIncreasedPrecision(i):
+    """Более точная производная в точке при задействии трёх узлов"""
     return (func(getXOnIteration(i + 1)) - func(getXOnIteration(i - 1))) / (2 * h)
-
-# Производные в граничных точках
 
 
 def leftExtremeDerivative():
+    """Производная в левой граничной точке"""
     return (-3 * func(getXOnIteration(0)) + 4 * func(getXOnIteration(1)) - func(getXOnIteration(2))) / (2 * h)
 
 
 def rightExtremeDerivative():
+    """Производная в правой граничной точке"""
     return (func(getXOnIteration(n - 2)) - 4 * func(getXOnIteration(n - 1)) + 3 * func(getXOnIteration(n))) / (2 * h)
 
 # --Элементарные интегралы--
 # У всех должен быть только один входной парамер (i)
 
-# Метод левых прямоугольников
-
 
 def leftElementaryIntegral(i):
+    """Метод левых прямоугольников"""
     return h * func(getXOnIteration(i - 1))
-
-# Метод правых прямоугольников
 
 
 def rightElementaryIntegral(i):
+    """Метод правых прямоугольников"""
     return h * func(getXOnIteration(i))
-
-# Метод центральных прямоугольников
 
 
 def centralElementaryIntegral(i):
+    """Метод центральных прямоугольников"""
     return h * func(getXOnIteration(i - 0.5))
-
-# Формула трапеции
 
 
 def trapezoidElementaryIntegral(i):
+    """Формула трапеции"""
     return (h / 2) * (func(getXOnIteration(i - 1)) + func(getXOnIteration(i)))
-
-# Формула симпсона
 
 
 def simpsonElementaryIntegral(i):
+    """Формула Симпсона"""
     return (h / 6) * (func(getXOnIteration(i)) + 4 * func(getXOnIteration(i - 0.5)) + func(getXOnIteration(i)))
 
 # --Интегралы--
 
-# Этот интеграл находится через сумму элементарных интегралов
-
 
 def integral(elementaryIntegral):
+    """Находение интеграла через сумму элементарных интегралов"""
     sum = 0
     for i in range(1, n + 1):
         sum += elementaryIntegral(i)
