68bece171ff20d2afc02b990a764543b4309dfa7
Iamnotagenius <kazakk9@gmail.com>
2023-10-21 01:20:23 +0300

    parse problem (really not ready yet)

--- lab1/simplex.py
+++ lab1/simplex.py
@@ -1,8 +1,49 @@
 import numpy as np
 
-def simplex(A, b, f):
+from json import loads
+
+example = """{"f": [1, 2, 3],
+              "goal": "max",
+              "constraints": [{"coefs": [1, 0, 0],
+                                "type": "eq",
+                                "b": 1},
+                               {"coefs": [1, 1, 0],
+                                "type": "gte",
+                                "b": 2},
+                               {"coefs": [1, 1, 1],
+                                "type": "lte",
+                                "b": 3}]}"""
+
+def parse_problem(json: str):
+    parsed = loads(json)
+    f = np.array(parsed['f'])
+    if parsed['goal'] == "min":
+        f *= -1
+    A = []
+    b = []
+    helper_vars = []
+    for i, constraint in enumerate(parsed['constraints']):
+        A.append(np.array(constraint['coefs']))
+        b.append(constraint['b'])
+        if constraint['type'] == "gte":
+            A[-1] *= -1
+            b[-1] *= -1
+            helper_vars.append(i)
+        if constraint['type'] == "lte":
+            helper_vars.append(i)
+    A = np.array(A)
+    helper_matrix = np.zeros((A.shape[0], len(helper_vars)))
+    for i, j in enumerate(helper_vars):
+        helper_matrix[j, i] = 1
+    A = np.hstack((A, np.array(helper_matrix)))
+    b = np.array(b)
+    return f, A, b
+
+print(*parse_problem(example), sep='\n')
+
+def simplex(f, A, b):
     tableau = np.hstack((b, A))
-    tableau = np.stack((tableau, f))
+    tableau = np.stack((tableau, np.hstack((0, f))))
     s = tableau[-1].argmax()
     while s > 0:
         ratios = tableau[:, 0] / tableau[:, s]
@@ -15,3 +56,4 @@ def simplex(A, b, f):
         s = tableau[-1].argmax()
 
 
+# TODO: улучшить поиск опорного плана, вынести шаг симплекса в отдельную функцию
