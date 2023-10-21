f0ed7640392bfe7b0610f51ed3d166038d0c3f45
Iamnotagenius <kazakk9@gmail.com>
2023-10-21 17:55:58 +0300

    simplex implementation

--- lab1/simplex.py
+++ lab1/simplex.py
@@ -21,39 +21,57 @@ def parse_problem(json: str):
         f *= -1
     A = []
     b = []
-    helper_vars = []
-    for i, constraint in enumerate(parsed['constraints']):
+    for constraint in parsed['constraints']:
         A.append(np.array(constraint['coefs']))
         b.append(constraint['b'])
         if constraint['type'] == "gte":
             A[-1] *= -1
             b[-1] *= -1
-            helper_vars.append(i)
-        if constraint['type'] == "lte":
-            helper_vars.append(i)
+        if constraint['type'] == "eq":
+            A.append(np.array(constraint['coefs']) * -1)
+            b.append(-constraint['b'])
     A = np.array(A)
-    helper_matrix = np.zeros((A.shape[0], len(helper_vars)))
-    for i, j in enumerate(helper_vars):
-        helper_matrix[j, i] = 1
-    A = np.hstack((A, np.array(helper_matrix)))
+    A = np.hstack((A, np.eye(A.shape[0])))
     b = np.array(b)
+    b = b.reshape((b.shape[0], 1))
     return f, A, b
 
-print(*parse_problem(example), sep='\n')
 
 def simplex(f, A, b):
     tableau = np.hstack((b, A))
-    tableau = np.stack((tableau, np.hstack((0, f))))
+    tableau = np.vstack((tableau, np.hstack((np.zeros((1,)), f, np.zeros((A.shape[0]))))))
+    print(tableau)
+    if b[b < 0].any():
+        i = tableau[:-1, 0].argmin()
+        l = tableau[i, 1:].argmin()
+        if tableau[i, l] >= 0:
+            raise Exception("No solution.")
+        ratios = tableau[:-1, 0] / tableau[:-1, l]
+        r = ratios.argmin()
+        simplex_step(tableau, r, l)
+        print(tableau)
     s = tableau[-1].argmax()
+
+    last_max = -1
     while s > 0:
-        ratios = tableau[:, 0] / tableau[:, s]
+        print(tableau[:-1, s])
+        temp = tableau[:-1, s]
+        temp[temp == 0] = -1
+        ratios = tableau[:-1, 0] / temp
         j = ratios.argmin()
-        for i in range(tableau.shape[0]):
-            if i == j:
-                tableau[i] /= tableau[i, s]
-                continue
-            tableau[i] = tableau[j] * -(tableau[j, s] / tableau[i, s]) + tableau[i]
+        simplex_step(tableau, j, s)
+        print(tableau)
         s = tableau[-1].argmax()
+        if tableau[-1, s] == last_max:
+            break
+        last_max = tableau[-1, s]
+    return tableau[-1, 0]
 
+def simplex_step(tableau, r, l):
+    for i in range(tableau.shape[0]):
+        if i == r:
+            tableau[i] /= tableau[i, l]
+            continue
+        tableau[i] -= tableau[r] * tableau[i, l] / tableau[r, l]
 
-# TODO: улучшить поиск опорного плана, вынести шаг симплекса в отдельную функцию
+print(simplex(*parse_problem(example)))
