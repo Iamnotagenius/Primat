ff7beefe581da76c85936d98a14b708d31429969
Sherka <obidjanow@mail.ru>
2023-12-06 18:19:21 +0300

    add return delete lines in reduse_matrix

--- lab2/game_theory.py
+++ lab2/game_theory.py
@@ -53,16 +53,20 @@ def simplefy_matrix(matrix):
 
 def reduce_matrix(matrix):
     abbreviated = True
+    deleteLines = []
+    deleteColumn = []
 
     while abbreviated:
+        ilines = list(set(range(matrix.shape[0]))-set(deleteLines))
+        icolumns = list(set(range(matrix.shape[1]))-set(deleteColumn))
+
         abbreviated = False
-        deleteLines = []
-        for iline1 in range(0, matrix.shape[0]):
-            for iline2 in range(0, matrix.shape[0]):
+        for iline1 in ilines:
+            for iline2 in ilines:
                 is_reduce = True
                 if iline1 == iline2:
                     continue
-                for column in range(0, matrix.shape[1]):
+                for column in icolumns:
                     if not matrix[iline1][column] >= matrix[iline2][column]:
                         is_reduce = False
                         break
@@ -71,15 +75,13 @@ def reduce_matrix(matrix):
                 else:
                     deleteLines.append(iline2)
                     abbreviated = True
-        matrix = np.delete(matrix, deleteLines, axis=0)
         
-        deleteColumn = []
-        for icolumn1 in range(0, matrix.shape[1]):
-            for icolumn2 in range(0, matrix.shape[1]):
+        for icolumn1 in icolumns:
+            for icolumn2 in icolumns:
                 is_reduce = True
                 if icolumn1 == icolumn2:
                     continue
-                for iline in range(0, matrix.shape[0]):
+                for iline in ilines:
                     if not matrix[iline][icolumn1] >= matrix[iline][icolumn2]:
                         is_reduce = False
                         break
@@ -88,13 +90,14 @@ def reduce_matrix(matrix):
                 else:
                     deleteColumn.append(icolumn1)
                     abbreviated = True
-        matrix = np.delete(matrix, deleteColumn, axis=1)
-    
-    return matrix
+    matrix = np.delete(matrix, deleteColumn, axis=1)
+    matrix = np.delete(matrix, deleteLines, axis=0)
+    return matrix, sorted(deleteLines), sorted(deleteColumn)
 
 
 # json_string = '{"matrix": [[4,5,6,7], [3,4,6,5], [7,6,10,8], [8,5,4,3]]}'
-json_string = '{"matrix": [[-20,-10,0,10], [-30,-20,0,-10], [10,0,40,20], [20,-10,-20,-30]]}'
+# json_string = '{"matrix": [[-20,-10,0,10], [-30,-20,0,-10], [10,0,40,20], [20,-10,-20,-30]]}'
+json_string = '{"matrix":[[10, 4, 11, 7],[7,6, 8,20],[6,2,1,11]]}'
 try:
     result_matrix = parse_problem(json_string)
     result_matrix = simplefy_matrix(result_matrix)
