3b7ba429cce0c2e0d59268b75866a256498ce3c6
froggeisnotagoodprogge <91646009+froggeisnotagoodprogge@users.noreply.github.com>
2023-12-13 08:15:08 +0300

    smthinthe way dumped

--- lab2/game_theory.py
+++ lab2/game_theory.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from scipy.optimize import linprog
 
 def parse_problem(json_str):
     try:
@@ -103,27 +104,42 @@ def strategies(matrix, deleteLines, deleteColumn):
             Pa.append(0) if i in deleteLines else Pa.append(1)
         for i in range(len(deleteColumn) + 1):
             Qb.append(0) if i in deleteColumn else Qb.append(1)
-#   else:
-#   !!! ТУТ ДОЛЖНО БЫТЬ ПРОДОЛЖЕНИЕ
-# 
-#   Должны быть рассмотрены случаи, когда невозможно решение в чистых стратегиях
-# 
-#   В качестве входных данных:
-#       matrix - уже сокращенная матрица
-#       deleteLines, deleteColumn - номера строк и столбцов которые были удалены в первоначальной матрице
-# 
-#   В Pa индексы которые есть в delteLines нужно оприравнять нулю, а в Qb индексы deleteColumn
-#       остальные индексы должны быть расчитаны с помощью симплекс метода
+    else:
+        f = np.ones(matrix.shape[1])
+        b = np.ones(matrix.shape[0])
+        result = linprog(c=f, A_eq=r_matrix, b_eq=b)
+        if result.success:
+            strategy_2 = result.x
+            strategy_1 = r_matrix.dot(strategy_2)
+            game_value = 1 / result.fun
+            strategy_2 = [x / sum(strategy_2) for x in strategy_2]
+            strategy_1 = [x / sum(strategy_1) for x in strategy_1]
+            k=0
+            for i in range(len(deleteLines) + len(strategy_1)):
+                if i in deleteLines: Pa.append(0)
+                else:
+                    Pa.append(strategy_1[k])
+                    k+=1
+            k=0
+            for i in range(len(deleteColumn) + len(strategy_2)):
+                if i in deleteColumn: Qb.append(0)
+                else:
+                    Qb.append(strategy_2[k])
+                    k+=1
+
+
 
     return Pa, Qb
 
 # json_string = '{"matrix": [[4,5,6,7], [3,4,6,5], [7,6,10,8], [8,5,4,3]]}'
 # json_string = '{"matrix": [[-20,-10,0,10], [-30,-20,0,-10], [10,0,40,20], [20,-10,-20,-30]]}'
-json_string = '{"matrix":[[10,4,11,7],[7,6,8,20],[6,2,1,11]]}'
+# json_string = '{"matrix":[[10,4,11,7],[7,6,8,20],[6,2,1,11]]}'
+json_string = '{"matrix":[[5,-8,7,-6,0], [8,-5,9,-3,2],[-2,7,-3,6,-4]]}'
 try:
     matrix = parse_problem(json_string)
     matrix = simplefy_matrix(matrix)
     r_matrix, deleteLines, deleteColumn = reduce_matrix(matrix)
+    print(r_matrix, deleteLines, deleteColumn)
     result = strategies(r_matrix, deleteLines, deleteColumn)
     print(result) #вывод: ([0, 1, 0], [0, 1, 0, 0])
 
